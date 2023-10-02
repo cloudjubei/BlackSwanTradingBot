d8ac0e9b84be67f52fd6c787875fb7cb8ae69853
Kamil Chmurzynski <cloudjubei@gmail.com>
2023-10-02 11:58:45 +0200

    USDT fix.

--- src/logic/signals/signals.module.ts
+++ src/logic/signals/signals.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common'
 import { SignalsService } from './signals.service'
-import { SignalsController } from './signals.controller'
 
 @Module({
-    controllers: [SignalsController],
     providers: [SignalsService],
     exports: [SignalsService],
 })
